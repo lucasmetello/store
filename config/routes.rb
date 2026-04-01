a21e620f07eec9f286eea193d9dfd34c959d7849
Lucas Meetello <lucasmetello@gmail.com>
2026-04-01 16:02:58 -0300

    Basic Inventory Tracking / subscribers to product partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   root "products#index"
-  resources :products
+
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
 end
