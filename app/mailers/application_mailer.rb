2d06d5ced1a7044da841b4f106fb10d8dfcc410e
Lucas Metello <lucasmetello@gmail.com>
2026-04-05 09:54:14 -0300

    Extracting a Concern

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  def in_stock
+    @product=params[:product]
+    mail to: params[:subscriber].email
+  end
 end
