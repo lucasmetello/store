840e11e238185372a7fc4430dee5ab135da2b783
Lucas Meetello <lucasmetello@gmail.com>
2026-04-07 15:54:53 -0300

    Testing Emails

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -2,9 +2,13 @@ require "test_helper"
 
 class ProductMailerTest < ActionMailer::TestCase
   test "in_stock" do
-    mail = ProductMailer.in_stock
+    product    = Product.new(name: "T-Shirt")
+    subscriber = User.new(email: "chris@example.org")
+
+    mail = ProductMailer.with(product: product, subscriber: subscriber).in_stock
+
     assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    assert_equal [ "chris@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
     assert_match "Hi", mail.body.encoded
   end
